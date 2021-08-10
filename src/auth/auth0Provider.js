fe9ee012fc1b9c70138c3cd945ba86cc4d440c91
taylorsieling <taylorasieling@gmail.com>
2021-08-09 19:44:53 -0500

    adding Auth0 files

---
+++ src/auth/auth0Provider.js
@@ -0,0 +1,27 @@
+
+import React from "react";
+import { useHistory } from "react-router-dom";
+import { Auth0Provider } from "@auth0/auth0-react";
+
+const Auth0ProviderWithHistory = ({ children }) => {
+  const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+  const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+
+  const history = useHistory();
+
+  const onRedirectCallback = (appState) => {
+    history.push( appState && appState.targetUrl ? appState.targetUrl : window.location.href = "http://localhost:3000/dashboard" ); };
+
+  return (
+    <Auth0Provider
+      domain={domain}
+      clientId={clientId}
+      redirectUri={window.location.origin}
+      onRedirectCallback={onRedirectCallback}
+    >
+      {children}
+    </Auth0Provider>
+  );
+};
+
+export default Auth0ProviderWithHistory;
