f4c14cf9966d47972f952afc059bbd93dd35b198
taylorsieling <taylorasieling@gmail.com>
2021-08-09 19:48:20 -0500

    creating user dashboard comps

---
+++ src/containers/Dashboard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import UserProfile from '../components/UserProfile'
+
+export default function Dashboard() {
+    return (
+        <div>
+            <h1>DASH</h1>
+            <UserProfile/>
+        </div>
+    )
+}
