8953a7f52f8a43aab7a4ff8cbf2743567fe7372c
taylorsieling <taylorasieling@gmail.com>
2021-08-15 21:53:56 -0500

    styling footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,10 +2,14 @@ import React from 'react'
 
 const Footer = () => {
     return (
-           <div className="footer">
-               <div>© 2021 Taylor Sieling</div>
-               <div>Forever Foster</div>
+        <nav class="flex items-center justify-between p-6 h-20 text-white shadow-sm bg-opal px-20">
+            <div>
+                <span className="text-xl tracking-tight font-display justify-start">Forever Foster</span>
             </div>
+            <div>
+                <span className="text-lg tracking-tight justify-start">&copy;2021 Taylor Sieling</span>
+            </div>
+        </nav>
     )
 }
 
