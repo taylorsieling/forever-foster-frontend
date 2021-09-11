6f742894ce594726c926b51907de88735397e24f
taylorsieling <taylorasieling@gmail.com>
2021-09-10 21:03:36 -0500

    building side nav on dashboard

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -3,13 +3,9 @@ import UserNav from '../components/UserNav'
 
 export default function Dashboard() {
     return (
-        <div>
-            <div className="dashboard">
-                <div className="container m-8 p-8"><UserNav/></div>
-                <div><h2>yada yada yada yada</h2></div>
-            </div>
-            
-            
+        <div className="flex">
+            <div className="min-h-screen max-w-xs w-2 flex flex-col flex-auto bg-mint antialiased text-blkgray"><UserNav/></div>
+            <div className="flex-grow rounded-3xl bg-white"><h2>yada yada yada yada</h2></div>
         </div>
     )
 }
